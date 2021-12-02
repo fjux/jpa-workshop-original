e9bfe634ce7b2a2c0c519fa82ec56dd50214cc0a
fjux <erikalfredsson1@gmail.com>
2021-12-02 13:02:38 +0100

    Added author class and then added fields to both book class and author class.

---
+++ src/main/java/com/example/jpaworkshoporiginal/model/Author.java
@@ -0,0 +1,51 @@
+package com.example.jpaworkshoporiginal.model;
+
+import javax.persistence.*;
+import java.util.Set;
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private int authorId;
+    private String firstName;
+    private String lastName;
+    @ManyToMany(mappedBy = "authors")
+    @JoinTable(name = "authors_books", joinColumns = @JoinColumn(name = "author_id"), inverseJoinColumns = @JoinColumn(name = "book_id"))
+    private Set<Book> writtenBooks;
+
+    public Author() {
+    }
+
+    public int getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(int authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getWrittenBooks() {
+        return writtenBooks;
+    }
+
+    public void setWrittenBooks(Set<Book> writtenBooks) {
+        this.writtenBooks = writtenBooks;
+    }
+}
--- src/main/java/com/example/jpaworkshoporiginal/model/Book.java
+++ src/main/java/com/example/jpaworkshoporiginal/model/Book.java
@@ -1,6 +1,7 @@
 package com.example.jpaworkshoporiginal.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -12,15 +13,27 @@ public class Book {
     private String isbn;
     private String title;
     private int maxLoanDays;
+    @ManyToMany
+    @JoinTable(name = "authors_books", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
 
     public Book() {
     }
 
-    public Book(int bookId, String isbn, String title, int maxLoanDays) {
+    public Book(int bookId, String isbn, String title, int maxLoanDays, Set<Author> authors) {
         this.bookId = bookId;
         this.isbn = isbn;
         this.title = title;
         this.maxLoanDays = maxLoanDays;
+        this.authors = authors;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 
     public int getBookId() {
